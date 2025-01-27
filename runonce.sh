615aecce5359b0ce5a1a439e642261f3defd14ba
Arjit Jaiswal <arjitj2@gmail.com>
2025-01-27 02:24:20 -0500

    Restore bash

--- runonce.sh
+++ runonce.sh
@@ -1,3 +1,5 @@
+#!/bin/bash -x
+
 aws ec2 modify-instance-attribute --no-source-dest-check \
   --region "$(/opt/aws/bin/ec2-metadata -z  | sed 's/placement: \(.*\).$/\1/')" \
   --instance-id "$(/opt/aws/bin/ec2-metadata -i | cut -d' ' -f2)"
