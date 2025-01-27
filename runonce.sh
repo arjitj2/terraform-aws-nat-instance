e6c628a2d99adee29187db5a9a5fb88d50589102
Arjit Jaiswal <arjitj2@gmail.com>
2025-01-26 20:57:33 -0500

    Just use one IP for this NAT instance

--- runonce.sh
+++ runonce.sh
@@ -9,7 +9,7 @@ aws ec2 modify-instance-attribute --no-source-dest-check \
 aws ec2 attach-network-interface \
   --region "$(/opt/aws/bin/ec2-metadata -z  | sed 's/placement: \(.*\).$/\1/')" \
   --instance-id "$(/opt/aws/bin/ec2-metadata -i | cut -d' ' -f2)" \
-  --device-index 1 \
+  --device-index 0 \
   --network-interface-id "${eni_id}"
 
 # start SNAT
